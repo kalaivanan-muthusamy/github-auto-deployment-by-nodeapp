7ed5c927afd4609b87909b60db0fa245901f3b66
Kalaivanan Muthusamy <kalaivanan.muthusamy97@gmail.com>
2020-07-15 02:18:55 +0530

    Config changes

--- configs.js
+++ configs.js
@@ -1,11 +1,23 @@
-exports.GITHUB_REPOSITORY_FULL_NAME = "bringmealsbackend/bringmeals-api";
-exports.BRANCHES_TO_DEPLOY = "refs/heads/develop";
-exports.TRIGGER_CONFIGURATIONS = "ALL";
-exports.APPLICATION_PATH_ON_SERVER = "/home/ubuntu/bringmeals/api";
-exports.SHELL_FILE_PATH_EXECUTE = "api_deployment.sh";
-exports.PORT_TO_RUN_THE_APP = "9001";
-exports.ENDPOINT_FOR_WEBHOOK = "hooks";
-exports.UNIQUE_RANDOM_STRING = "GyzdX4fVRZgSe6VsGfiD";
-exports.GITHUB_CONTENT_TYPE = "application/json";
+const GITHUB_REPOSITORY_FULL_NAME = "bringmealsbackend/bringmeals-api";
+const BRANCHES_TO_DEPLOY = "refs/heads/develop";
+const TRIGGER_CONFIGURATIONS = "ALL";
+const APPLICATION_PATH_ON_SERVER = "/home/ubuntu/bringmeals/api";
+const SHELL_FILE_PATH_EXECUTE = "api_deployment.sh";
+const PORT_TO_RUN_THE_APP = "9001";
+const ENDPOINT_FOR_WEBHOOK = "hooks";
+const UNIQUE_RANDOM_STRING = "GyzdX4fVRZgSe6VsGfiD";
+const GITHUB_CONTENT_TYPE = "application/json";
+const POST_URL = `/${ENDPOINT_FOR_WEBHOOK}/${UNIQUE_RANDOM_STRING}`;
 
-exports.POST_URL = `/${ENDPOINT_FOR_WEBHOOK}/${UNIQUE_RANDOM_STRING}`;
+module.exports = {
+  GITHUB_REPOSITORY_FULL_NAME,
+  BRANCHES_TO_DEPLOY,
+  TRIGGER_CONFIGURATIONS,
+  APPLICATION_PATH_ON_SERVER,
+  SHELL_FILE_PATH_EXECUTE,
+  PORT_TO_RUN_THE_APP,
+  ENDPOINT_FOR_WEBHOOK,
+  UNIQUE_RANDOM_STRING,
+  GITHUB_CONTENT_TYPE,
+  POST_URL,
+};
